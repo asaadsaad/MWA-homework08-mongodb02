127abab2342549de539dfdc97a865f0852fa460f
Asaad Saad <asaad@outlook.com>
2023-07-02 16:32:19 -0500

    Delete questions.model.ts

--- models/questions.model.ts
+++
@@ -1,8 +0,0 @@
-import { InferSchemaType, Schema } from 'mongoose';
-
-export const questionSchema = new Schema({
-    question: String,
-    due_date: Number
-});
-
-export type IQuestion = InferSchemaType<typeof questionSchema>;
